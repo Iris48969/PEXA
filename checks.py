11d845c6ad333978b59cc990308a78161a5935c9
anuari1999 <anuraag.dasari@student.unimelb.edu.au>
2024-08-28 12:22:46 +1000

    Update checks.py

--- checks.py
+++ checks.py
@@ -6,6 +6,13 @@ import pandas as pd
 import logging
 
 def births_region_level_sum_check(conn):
+    """
+    The purpose of this function is to call an SQL script which sums the births of all FAs in a particular SA2 and checks
+    if this value matches the total births of the SA2
+
+    Input: Connection
+    Output: Table which contains information of SA2s that failed the check (Code | Region Type | Description)
+    """
     try:
         sql = open(os.path.abspath("SQL_Queries/Births Sum Check for FA vs SA2 Output.sql"), 'r').read()
         logging.info("Births region level sum check query opened")
@@ -16,6 +23,13 @@ def births_region_level_sum_check(conn):
         logging.error(e)
 
 def deaths_region_level_sum_check(conn):
+    """
+    The purpose of this function is to call an SQL script which sums the deaths of all FAs in a particular SA2 and checks
+    if this value matches the total births of the SA2
+
+    Input: Connection
+    Output: Table which contains information of SA2s that failed the check (Code | Region Type | Description)
+    """
     try:
         sql = open(os.path.abspath("SQL_Queries/Deaths Sum Check for FA vs SA2 Output.sql"), 'r').read()
         logging.info("Deaths region level sum check query opened")
@@ -26,6 +40,13 @@ def deaths_region_level_sum_check(conn):
         logging.error(e)
 
 def household_region_level_sum_check(conn):
+    """
+    The purpose of this function is to call an SQL script which sums the households of all FAs in a particular SA2 and checks
+    if this value matches the total births of the SA2
+
+    Input: Connection
+    Output: Table which contains information of SA2s that failed the check (Code | Region Type | Description)
+    """
     try:
         sql = open(os.path.abspath("SQL_Queries/Household Sum Check for FA vs SA2 Output.sql"), 'r').read()
         logging.info("Household region level sum check query opened")
@@ -36,6 +57,13 @@ def household_region_level_sum_check(conn):
         logging.error(e)
 
 def population_region_level_sum_check(conn):
+    """
+    The purpose of this function is to call an SQL script which sums the population of all FAs in a particular SA2 and checks
+    if this value matches the total births of the SA2
+
+    Input: Connection
+    Output: Table which contains information of SA2s that failed the check (Code | Region Type | Description)
+    """
     try:
         sql = open(os.path.abspath("SQL_Queries/Population Sum Check for FA vs SA2 Output.sql"), 'r').read()
         logging.info("Population region level sum check query opened")
